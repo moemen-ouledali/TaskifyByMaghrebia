888a822133d04fe6a3b0283d37be556ca1b1415d
Moemen Ouled Ali <148381643+moemen-ouledali@users.noreply.github.com>
2026-02-20 14:57:14 +0100

    removed unnecessary test files

--- BACKEND/src/app.module.ts
+++ BACKEND/src/app.module.ts
@@ -1,19 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { TestModule } from './test.module';
 import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI!),
-    TestModule, 
-     UsersModule,
+    UsersModule,
     AuthModule,
   ],
-  controllers: [AppController], // add root controller
-  providers: [],
+  controllers: [AppController],
 })
 export class AppModule {}
