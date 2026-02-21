7de8fcd1cbcd570960cb8a13064fb0f1165e0e7a
Moemen Ouled Ali <148381643+moemen-ouledali@users.noreply.github.com>
2026-02-21 14:08:20 +0100

    auth not Auth

--- FRONTEND/src/App.tsx
+++ FRONTEND/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Auth/Login";
+import Login from "./pages/auth/Login";
 
 function DashboardPlaceholder() {
   return (
